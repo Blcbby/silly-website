b63b31f8e7409883038e55ff5ad49c12ccb4696f
Blcbby <staticelectricity986@gmail.com>
2023-05-24 23:26:04 -0400

    24-may-23 work

--- aaaa.js
+++ aaaa.js
@@ -12,3 +12,27 @@ function test() {
     }
 }
 setTimeout(test, 0)
+
+var cookieClicks = 0;
+
+function yummyNummy() {
+    if (cookieClicks == 0) {
+        alert("cookie!");
+        alert("that's it.");
+    } else if (cookieClicks == 1) {
+        alert("why did you click it again?");
+        alert("please stop.");
+    } else if (cookieClicks == 2) {
+        alert("please.");
+    } else if (cookieClicks == 3) {
+        alert("i am asking you nicely.");
+    } else if (cookieClicks == 4) {
+        alert("if you do not stop, i will have to get...");
+        alert("nasty...")
+    } else if (cookieClicks == 5) {
+        alert("alright, you asked for it...");
+        window.location.pathname = "/bad.png";
+    }
+    console.log(cookieClicks)
+    cookieClicks++;
+}
